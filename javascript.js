190c21c95def8e3737c78164199b42655ad75bc2
johanarrhen <arrhen.98@gmail.com>
2017-04-03 00:53:07 +0200

    fixerade toppen och började experimentera med scroll

--- javascript.js
+++ javascript.js
@@ -1,7 +1,15 @@
 // JavaScript Document
 
 window.onload = function() {
-  
-  
-  
+
+
+
 };
+function scroll(e){
+  console.log("scroll");
+  
+}
+
+
+
+document.addEventListener("scroll", scroll);
