af8e3777a203eac2d31f093c3736c8fba3718f43
johanarrhen <arrhen.98@gmail.com>
2017-04-03 18:50:32 +0200

    samtliga värden i #topp är ändrade till rems med px som backup om det inte finns till webläsaren. #topp blir mindre vid 200px scroll och fungerar på alla värden där font-size för html ändras

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-
+var storRubrik = true;
 window.onload = function() {
 
 
@@ -7,18 +7,43 @@ window.onload = function() {
 };
 
 
-function scroll(e){
-  var scrollPosition = $(window).scrollTop();
-  console.log(scrollPosition);
 
-  if (scrollPosition > 200){
-    $("#topp").css("height", "30px");
-  }else{
-    $("#topp").css("height", "50px");
+
+function scroll(){
+  var scrollPosition = $(window).scrollTop(); //hur långt det är scrollat i webbläsaren
+
+
+
+  if(scrollPosition > 200 && storRubrik == true){ //scrollat mer än 200px men rubriken är stor
+    $("#huvudrubrik").animate({"font-size": "3rem"}, 100);
+    $("#topp").animate({"height": "3rem"}, 100);
+    storRubrik = false;
+  }
+  if(scrollPosition < 200 && storRubrik != true){ //scrollat mindre än 200px men rubriken är inte stor
+
+    $("#huvudrubrik").animate({"font-size": "6rem"}, 100);
+    $("#topp").animate({"height": "6rem"}, 100);
+    storRubrik = true;
   }
 
-}
 
+
+
+
+
+
+  // if (scrollPosition === 200 || scrollPosition - 1 === ){
+  //   $("#topp").css("height", "30px");
+  //   // $("#huvudrubrik").css("font-size", 3 + "rem")
+  //   $("#huvudrubrik").animate({"font-size": "3rem"}, 1000);
+  //   // $("nav").css("padding-bottom", 0.2 + "rem")
+  // }else{
+  //   $("#topp").css("height", "60px");
+  //   // $("#huvudrubrik").css("font-size", 6 + "rem") //tilbaka till ursprungsläget
+  //   $("#huvudrubrik").animate({"font-size": "6rem"}, 1000);
+  //   // $("nav").css("padding-bottom", 0.4 + "rem")
+  // }
+}
 
 
 document.addEventListener("scroll", scroll);
