f3c44dc069d251d8f38f709f1bf539fa7510f64d
johanarrhen <arrhen.98@gmail.com>
2017-04-03 14:31:14 +0200

    scrollTop() gör att höjden på #topp ändras vid 200px

--- javascript.js
+++ javascript.js
@@ -5,9 +5,18 @@ window.onload = function() {
 
 
 };
+
+
 function scroll(e){
-  console.log("scroll");
-  
+  var scrollPosition = $(window).scrollTop();
+  console.log(scrollPosition);
+
+  if (scrollPosition > 200){
+    $("#topp").css("height", "30px");
+  }else{
+    $("#topp").css("height", "50px");
+  }
+
 }
 
 
