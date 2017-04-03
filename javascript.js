41e01f4619bafed6afceffc24cbc0443834f7821
johanarrhen <arrhen.98@gmail.com>
2017-04-04 01:54:16 +0200

    scrollfunktionen anpassad för att endast fungera då skärmen är större än ett visst antal pixlar

--- javascript.js
+++ javascript.js
@@ -11,9 +11,13 @@ window.onload = function() {
 
 function scroll(){
   var scrollPosition = $(window).scrollTop(); //hur långt det är scrollat i webbläsaren
+  var width = $(window).width();
 
+  if(width >= 800){
+    toppHeight();
+  }
 
-
+function toppHeight(){
   if(scrollPosition > 200 && storRubrik == true){ //scrollat mer än 200px men rubriken är stor
     $("#huvudrubrik").animate({"font-size": "3rem"}, 100);
     $("#topp").animate({"height": "3rem"}, 100);
@@ -25,6 +29,7 @@ function scroll(){
     $("#topp").animate({"height": "6rem"}, 100);
     storRubrik = true;
   }
+}
 
 
 
